093d2483ef72249b0736f4c0bdc84deef072124e
Irina Fileva <irinkaskai@gmail.com>
2024-09-20 15:44:40 +0500

    feat: add link in header and delete input

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,46 +1,59 @@
 'use client';
-import { AppShell, Autocomplete, Title } from '@mantine/core';
+import { FC, ReactNode } from 'react';
+import { Container, Title } from '@mantine/core';
 import MapSvg from 'shared/assets/svg/map.svg';
 import PhoneSvg from 'shared/assets/svg/phone.svg';
+import Clock from 'shared/assets/svg/clock.svg';
 import Logo from 'shared/assets/img/Logo.png';
 import styles from './Header.module.scss';
 import Image from 'next/image';
-import { serviceCategories } from 'shared/constants/serviceСategories';
 import Link from 'next/link';
 
-const largeData = serviceCategories.map((category) => category.name);
+type ProsHeader = {
+  children: ReactNode;
+};
 
-export const Header = () => {
+export const Header: FC<ProsHeader> = ({ children }) => {
   return (
-    <AppShell.Header className={styles.header}>
+    <header className={styles.header}>
       <Link className={styles.linkLogo} href={'/'}>
-        <Image src={Logo} alt="Logo" width={90} height={95} />
+        <Image src={Logo} alt="Logo" width={90} height={95} priority />
         <Title>{'« Любимцы в доме »'}</Title>
       </Link>
-      <Autocomplete
-        placeholder="Поиск..."
-        data={largeData}
-        maxDropdownHeight={200}
-        classNames={{ input: styles.textInput }}
-      />
-      <Link
-        className={styles.linkMap}
-        href={
-          'https://2gis.ru/ufa/firm/70000001029826447?m=56.047195%2C54.784976%2F16'
-        }
-        target="_blank"
-      >
-        <Image src={MapSvg} alt="map" />
-        <span>Как нас найти</span>
-      </Link>
-      <Link
-        className={styles.linkMap}
-        href={'tel:+79603938390'}
-        target="_blank"
-      >
-        <Image src={PhoneSvg} alt="phone" />
-        <span>Позвонить нам</span>
-      </Link>
-    </AppShell.Header>
+      <Container className={styles.rootLinks} visibleFrom="sm">
+        <div className={styles.titleContact}>
+          <Image src={MapSvg} alt="map" priority />
+          {'Адрес '}
+        </div>
+        <Link
+          className={styles.link}
+          href={
+            'https://2gis.ru/ufa/firm/70000001029826447?m=56.047195%2C54.784976%2F16'
+          }
+          target="_blank"
+        >
+          г. Уфа <br /> &nbsp; Уфимское шоссе, 25
+        </Link>
+      </Container>
+      <Container className={styles.rootLinks} visibleFrom="sm">
+        <div className={styles.titleContact}>
+          <Image src={Clock} alt="clock" priority />
+          {'Время работы'}
+        </div>
+        <span style={{ fontWeight: '600' }}>
+          Ежедневно <br /> с 10:00 до 21:00
+        </span>
+      </Container>
+      <Container className={styles.rootLinks}>
+        <div className={styles.titleContact}>
+          <Image src={PhoneSvg} alt="phone" priority />
+          <span>{'Телефон'} </span>
+        </div>
+        <Link className={styles.link} href={'tel:+79603938390'} target="_blank">
+          {'+7‒960‒393‒83‒90'}
+        </Link>
+      </Container>
+      {children}
+    </header>
   );
 };
