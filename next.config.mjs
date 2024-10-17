da2260920e1017867f414ca2f142287448527e47
Irina Fileva <irinkaskai@gmail.com>
2024-10-17 22:10:01 +0500

    chore: install swiper and embla-carousel-react

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   sassOptions: {
     prependData: `@import "./_mantine.scss";`,
   },
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+  },
 };
 
 export default nextConfig;
