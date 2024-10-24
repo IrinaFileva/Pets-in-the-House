11c2ddb3cf1be843247e562558b429df69c2b076
Irina Fileva <irinkaskai@gmail.com>
2024-10-24 10:22:38 +0500

    fix: simplify the code structure of the contact list

--- src/widgets/ContactsList/ui/ContactsList.tsx
+++ src/widgets/ContactsList/ui/ContactsList.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import { YMaps, Map, Placemark } from '@r3flector/react-yandex-maps';
 import { Container, List } from '@mantine/core';
 import { OUTSIDE_LINKS } from 'shared/constants';
 import { HeaderPage } from 'shared/ui/HeaderPage';
@@ -17,86 +17,55 @@ export const ContactsList = () => {
     <main className={styles.contactsSection}>
       <section className={styles.contacts}>
         <HeaderPage titlePage="Контакты" />
-        <Container className={styles.containerLists}>
-          <List
-            classNames={{
-              item: styles.listItem,
-              itemLabel: styles.itemLabel,
-            }}
+        <List
+          classNames={{
+            item: styles.listItem,
+            itemWrapper: styles.listItemWrapper,
+            itemLabel: styles.itemLabel,
+          }}
+        >
+          <List.Item
+            icon={<Image src={Phone} alt="phone" width={27} height={27} />}
           >
-            <List.Item
-              icon={<Image src={Phone} alt="phone" width={27} height={27} />}
-            >
-              {'Телефон:'}
-            </List.Item>
-            <List.Item
-              icon={
-                <Image src={Address} alt="address" width={27} height={27} />
-              }
-            >
-              {'Aдрес:'}
-            </List.Item>
-            <List.Item
-              icon={<Image src={Clock} alt="clock" width={27} height={27} />}
-            >
-              {'График:'}
-            </List.Item>
-            <List.Item
-              icon={<Image src={VK} alt="VK" width={27} height={27} />}
-            >
-              {'ВКонтакте:'}
-            </List.Item>
-            <List.Item
-              icon={
-                <Image src={Telega} alt="Telegram" width={27} height={27} />
-              }
-            >
-              {'Telegram:'}
-            </List.Item>
-          </List>
-          <List
-            classNames={{
-              item: styles.listItemLink,
-            }}
+            <p>{'Телефон:'}</p>
+            <Link href={OUTSIDE_LINKS.tel} target="_blank">
+              {' +7‒960‒393‒83‒90'}
+            </Link>
+          </List.Item>
+          <List.Item
+            icon={<Image src={Address} alt="address" width={27} height={27} />}
           >
-            <List.Item icon={<span />}>
-              <Link href={OUTSIDE_LINKS.tel} target="_blank">
-                {' +7‒960‒393‒83‒90'}
-              </Link>
-            </List.Item>
-            <List.Item icon={<span />}>
-              <Link href={OUTSIDE_LINKS.map} target="_blank">
-                {' г. Уфа Уфимское шоссе, 25'}
-              </Link>
-            </List.Item>
-            <List.Item icon={<span />}>
-              <p className={styles.clockTitle}>
-                {'Ежедневно, с 10:00 до 21:00'}
-              </p>
-            </List.Item>
-            <List.Item icon={<span />}>
-              <Link
-                className={styles.linkVk}
-                href={OUTSIDE_LINKS.vk}
-                target="_blank"
-              >
-                {' Любимцы в Доме'}
-              </Link>
-            </List.Item>
-            <List.Item icon={<span />}>
-              <Link
-                className={styles.linkVk}
-                href={OUTSIDE_LINKS.telegram}
-                target="_blank"
-              >
-                {' @lyubimtsyvdome'}
-              </Link>
-            </List.Item>
-          </List>
-        </Container>
+            <p>{'Aдрес:'}</p>
+            <Link href={OUTSIDE_LINKS.map} target="_blank">
+              {' г. Уфа Уфимское шоссе, 25'}
+            </Link>
+          </List.Item>
+          <List.Item
+            icon={<Image src={Clock} alt="clock" width={27} height={27} />}
+          >
+            <p>{'График:'}</p>
+            <span className={styles.clockTitle}>
+              {'Ежедневно, с 10:00 до 21:00'}
+            </span>
+          </List.Item>
+          <List.Item icon={<Image src={VK} alt="VK" width={27} height={27} />}>
+            <p>{'ВКонтакте:'}</p>
+            <Link href={OUTSIDE_LINKS.vk} target="_blank">
+              {' Любимцы в Доме'}
+            </Link>
+          </List.Item>
+          <List.Item
+            icon={<Image src={Telega} alt="Telegram" width={27} height={27} />}
+          >
+            <p>{'Telegram:'}</p>
+            <Link href={OUTSIDE_LINKS.telegram} target="_blank">
+              {' @lyubimtsyvdome'}
+            </Link>
+          </List.Item>
+        </List>
       </section>
       <Container className={styles.constantsMap}>
-        <YMaps>
+        <YMaps query={{ apikey: '7c780289-31c5-4a03-be19-cd48a8100e36' }}>
           <Map
             style={{
               position: 'relative',
